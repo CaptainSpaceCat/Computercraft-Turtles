4a397207f5ee5487977a948961ae4a2068b2623f
Connor Settle <csettle@stanford.edu>
2022-06-20 21:02:32 -0700

    know when its time to pee

--- orb.lua
+++ orb.lua
@@ -3,19 +3,30 @@ local Buffer = require "lib/buffer"
 local delay = 1
 local buf = Buffer("top", "front")
 
+function pee_ready()
+    return (
+        buf:count("emendatusenigmatica:uranium_ingot") >= 2 and
+        buf:count("emendatusenigmatica:fluorite_dust") >= 1 and
+        buf:count("emendatusenigmatica:sulfur_dust") >= 1
+    )
+end
+
 function urinate()
-    if buf:take("emendatusenigmatica:uranium_ingot", 2) ~= 2 then return false end
+    buf:take("emendatusenigmatica:uranium_ingot", 2)
     turtle.suck()
     turtle.dropDown()
-    if buf:take("emendatusenigmatica:fluorite_dust", 1) ~= 1 then return false end
+    buf:take("emendatusenigmatica:fluorite_dust", 1)
     turtle.suck()
     turtle.dropDown()
-    if buf:take("emendatusenigmatica:sulfur_dust", 1) ~= 1 then return false end
+    buf:take("emendatusenigmatica:sulfur_dust", 1)
     turtle.suck()
     turtle.dropDown()
 end
 
 while true do
+    while not pee_ready() do
+        sleep(10)
+    end
     urinate()
     sleep(delay)
 end
