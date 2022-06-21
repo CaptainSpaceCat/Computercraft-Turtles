a3501a724b5d14a31300659f53cecb6575471be5
Connor Settle <csettle@stanford.edu>
2022-06-20 20:57:32 -0700

    bugfixes

--- orb.lua
+++ orb.lua
@@ -1,18 +1,16 @@
 local Buffer = require "lib/buffer"
-local List = require "lib/list"
-local Recipe = require "lib/recipe"
 
 local delay = 1
 local buf = Buffer("top", "front")
 
 function urinate()
-    if buf.take("emendatusenigmatica:uranium_ingot", 2) ~= 2 then return false end
+    if buf:take("emendatusenigmatica:uranium_ingot", 2) ~= 2 then return false end
     turtle.suck()
     turtle.dropDown()
-    if buf.take("emendatusenigmatica:fluorite_dust", 1) ~= 1 then return false end
+    if buf:take("emendatusenigmatica:fluorite_dust", 1) ~= 1 then return false end
     turtle.suck()
     turtle.dropDown()
-    if buf.take("emendatusenigmatica:sulfur_dust", 1) ~= 1 then return false end
+    if buf:take("emendatusenigmatica:sulfur_dust", 1) ~= 1 then return false end
     turtle.suck()
     turtle.dropDown()
 end
