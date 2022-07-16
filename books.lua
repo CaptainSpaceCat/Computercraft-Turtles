aae805fef48661b7d6a0fe6e9618d513f6945653
Connor Settle <csettle@stanford.edu>
2022-07-16 15:38:17 -0700

    automated books of binding

---
+++ books.lua
@@ -0,0 +1,17 @@
+
+function waitToCraft()
+	rs.setAnalogOutput("top", 15)
+	while true do
+		if turtle.getItemCount(1) > 0 then
+			rs.setAnalogOutput("top", 0)
+			return true
+		end
+		sleep(5)
+	end
+end
+
+
+while waitToCraft() do
+	turtle.craft()
+	turtle.drop()
+end
